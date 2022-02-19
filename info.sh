099716931cb6a0df0ae8d7083ef7f1ea5df491ea
quang2002 <clone27112002@gmail.com>
2022-02-19 16:05:25 +0700

    .

--- info.sh
+++ info.sh
@@ -5,61 +5,32 @@ HOSTNAME=`hostname`
 echo "Ten may: $HOSTNAME"
 
 # ban phan phoi, phien ban
-info=$(cat /etc/*-release)
-for i in ${info[@]}; do
+IFS='='
+read -ra arr <<< `grep "^NAME=" /etc/os-release`
+echo "Phien ban phan phoi: ${arr[1]//\"/}"
 
-    IFS='="'
-    read -ra ADDR <<< "$i"
-
-    case ${ADDR[0]} in
-        DISTRIB_ID)
-            echo "Phien ban phan phoi: ${ADDR[1]}"
-            ;;
-        DISTRIB_RELEASE)
-            echo "Phien ban he dieu hanh: ${ADDR[1]}"
-            ;;
-    esac
-done
+read -ra arr <<< `grep "^VERSION_ID=" /etc/os-release`
+echo "Phien ban he dieu hanh: ${arr[1]//\"/}"
 
 # thong tin phan cung
-info=$(cat /proc/cpuinfo)
-IFS=$'\n'
-for i in ${info[@]}; do
-    IFS=':'
-    read -ra ADDR <<< "$i"
-    ADDR=`echo ${ADDR[0]} | xargs`
-    case ${ADDR[0]%%*( )} in
-        "model name")
-            echo "Ten CPU: ${ADDR[1]}"
-            ;;
-        "cpu MHz")
-            echo "Toc do: ${ADDR[1]} MHz"
-            ;;
-        "clflush size")
-            echo "CPU Bit: ${ADDR[1]} bit"
-            break
-            ;;
-    esac
-done
+IFS=':'
+
+read -ra arr <<< `grep "^model name" /proc/cpuinfo`
+echo "Ten CPU: ${arr[1]// /}"
+
+read -ra arr <<< `grep "^clflush size" /proc/cpuinfo`
+echo "CPU Bit: ${arr[1]// /} bit"
+
+read -ra arr <<< `grep "^cpu MHz" /proc/cpuinfo`
+echo "Toc do: ${arr[1]// /} MHz"
+
 
 # cat /proc/meminfo
-info=$(cat /proc/meminfo)
-IFS=$'\n'
-for i in ${info[@]}; do
-    IFS=':'
-    read -ra ADDR <<< "$i"
-    ADDR[0]=`echo ${ADDR[0]} | xargs`
-    ADDR[1]=`echo ${ADDR[1]} | xargs`
-    let ADDR[1]=`echo ${ADDR[1]:0:-2}`/1024
-
-    case ${ADDR[0]%%*( )} in
-        "MemTotal")
-            echo "Thong tin bo nho vat li (tong bao nhieu MB): ${ADDR[1]} MB"
-            break
-            ;;
-    esac
-done
-
+IFS=':'
+read -ra arr <<< `grep "^MemTotal:" /proc/meminfo`
+arr[1]=${arr[1]// /}
+arr[1]=${arr[1]:0:-2}
+echo "Thong tin bo nho vat li (tong bao nhieu MB): `expr ${arr[1]} / 1024` MB"
 
 # thong tin bo nho hard disk
 info=$(df -h)
@@ -93,3 +64,27 @@ printf "Bo nho trong: %.2f GB\n" ${totalFree/\./,}
 IP=$(curl -s ifconfig.me)
 echo 'IP:' $IP
 
+# list users
+info=$(grep "/home/.*:/usr/bin" /etc/passwd)
+IFS=$'\n'
+echo "Danh sach user dang co tren he thong:"
+for i in ${info[@]}; do
+    IFS=':'
+    read -ra ADDR <<< "$i"
+    echo "${ADDR[0]}"
+done
+
+# list all process running with root permission
+echo "Danh sach cac tien trinh dang chay co quyen root:"
+ps -ef | grep "^root"
+
+# information of open ports
+echo "Danh sach cac port dang mo:"
+netstat -tulpn | grep LISTEN
+
+#
+echo "Danh sach cac thu muc other co quyen ghi: "
+sudo find / -type d -perm /o=w
+
+echo "Danh sach cac ung dung da duoc cai:"
+apt list --installed
